b819c04f0a0b07f5ff161cf47cd135bcfdf97b6a
Lucas Hohmann <lfhohmann@gmail.com>
2022-09-11 12:52:08 -0300

    "say_bye" function implementation

--- src/lfhohmann/helloworld.py
+++ src/lfhohmann/helloworld.py
@@ -5,3 +5,9 @@ def say_hello(name: str = "world") -> str:
     """Basic HelloWorld function"""
 
     return f"Hello, {name}!"
+
+
+def say_bye(name: str = "world") -> str:
+    """Basic ByeWorld function"""
+
+    return f"Bye, {name}!"
--- tests/test_helloworld.py
+++ tests/test_helloworld.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from lfhohmann.helloworld import say_hello
+from lfhohmann.helloworld import say_bye, say_hello
 
 
 class SayHello(unittest.TestCase):
@@ -18,3 +18,18 @@ class SayHello(unittest.TestCase):
 
         self.assertEqual(say_hello("Lucas"), "Hello, Lucas!")
         self.assertEqual(say_hello("Lucas Hohmann"), "Hello, Lucas Hohmann!")
+
+
+class SayBye(unittest.TestCase):
+    """Test case for 'say_bye()' function"""
+
+    def test_no_name(self):
+        """Test function without a <name> argument"""
+
+        self.assertEqual(say_bye(), "Bye, world!")
+
+    def test_valid_name(self):
+        """Test function with <name> argument"""
+
+        self.assertEqual(say_bye("Lucas"), "Bye, Lucas!")
+        self.assertEqual(say_bye("Lucas Hohmann"), "Bye, Lucas Hohmann!")
